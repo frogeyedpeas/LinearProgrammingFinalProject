5113876e18c59200d2b4fb6d94b5f428f8d7ae2b
Sidharth Ghoshal <sidharthghoshal@nbp-86-175.nbp.ruw.rutgers.edu>
2016-05-05 22:43:16 -0400

    images

--- DiagonalMatrixScaling.py
+++ DiagonalMatrixScaling.py
@@ -1,22 +1,108 @@
 
 import numpy as np
+from numpy import linalg
+import matplotlib.pyplot as plt
 
 
+from mpl_toolkits.mplot3d import Axes3D
 
 
+def DiagonalNewton(d0,d1,Q,tolerance):
 
-def DiagonalNewton(d0,d1,Q,lamb,tolerance):
-    while(linalg.norm(d1 -d0) > tolerance):
+    if tolerance <= 0:
+            print "tolerance <= 0, algorithm won't terminate"
+            return None
+
+    #verify if Q is positive semidefinite, correct dimensions
+    try:
+        y = linalg.cholesky(Q)
+    except linalg.LinAlgError:
+        print "Matrix is either not positive semidefinite, or nonsquare"
+        return None
+    
+    w = linalg.norm(d1 -d0)
+    iterationcounter = 0
+    while(w > tolerance):
         d0 = d1
         D = np.diag(d1)
-        y = linalg.matrix_power((linalg.matrix_power(D,-2)+Q),-1)*(linalg.matrix_power(np.diag(D,-1) )-Q*d1)
-        lamb = (np.transpose(y) * linalg.matrix_power(D,-2)*y  + np.transpose(y)*Q*y)[0] #computing lambda
+
+
+        try:
+       
+            y = np.dot(linalg.matrix_power((linalg.matrix_power(D,-2)+Q),-1), np.diag( linalg.matrix_power(D,-1 ))-np.dot(Q,d1))
+
+        except:
+
+            return None
+      
+          
+       # print  np.dot(np.dot(np.transpose(y) ,linalg.matrix_power(D,-2)),y)
+        
+        lamb = np.dot(
+                    np.dot(np.transpose(y) ,linalg.matrix_power(D,-2)),
+                            y) + np.dot(np.dot(np.transpose(y),Q),y) #computing lambda
         #lamb < 1, a = 1 lamb >=1 1, a = 1/2
+        
+        #print lamb
         if lamb > 1:
-            d1 = d0 + float(1)/(1 + lamb)*y
+            d1 = d0 + float(1)/(1 + lamb**(0.5))*y
 
         else:
             d1 = d0 + y
+        w = linalg.norm(d1 -d0)
+
+        iterationcounter += 1
+
+    return (w, d1,iterationcounter)
+
+
+d0 = np.array([1,1])
+Q = np.array([[1,0],[0,2]])
+d1 = np.array([2,9])
+
+
+i = 0
+while i < 5:
+    j = 0
+    while j < 5:
+        if j != 0 and i !=0:
+            #rendering plots now:
+            k = 0
+            while k < 10:
+                X = []
+                Y = []
+                Z = []
+                h = 0
+                while h < 10:
+                    X.append(h)
+                    Y.append(k)
+                    teal = DiagonalNewton(np.array([0,0]),np.array([h,k]), np.diag(np.array([i,j])),.001)
+                    if teal != None:
+                        Z.append(teal[2])
+                    else:
+                        Z.append(0)
+                    h+=1
+    
+                k+=1
+            fig = plt.figure()
+            ax = plt.axes(projection='3d')
+            ax.plot_wireframe(X, Y, Z)
+            plt.savefig("Image: "+str(i)+ ","+str(j))
+            
+        j+=1
+    i+=1
+
+
+
+
+                    
+
+
+
+
+
+
+
 
 
 
